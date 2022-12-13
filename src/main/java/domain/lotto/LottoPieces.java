4962db32f5dae16e9973718f8faad0524e7fa4b1
이건회 <rg970604@naver.com>
2022-12-13 21:02:38 +0900

    refactor : LottoPieces.java

--- src/main/java/domain/lotto/LottoPieces.java
+++ src/main/java/domain/lotto/LottoPieces.java
@@ -2,18 +2,19 @@ package domain.lotto;
 
 public class LottoPieces {
 
-  public int pieces;
+    private final int MONEY_UNIT = 1000;
+    public int pieces;
 
-  public LottoPieces(int payment) {
-    validatePrice(payment);
-    int pieces = payment / 1000;
-    this.pieces = pieces;
-  }
-
-  private void validatePrice(int amount) {
-    if (amount % 1000 != 0) {
-      throw new IllegalArgumentException("1000원 단위로만 구매할 수 있습니다");
+    public LottoPieces(int payment) {
+        validatePrice(payment);
+        int pieces = payment / MONEY_UNIT;
+        this.pieces = pieces;
     }
-  }
+
+    private void validatePrice(int amount) {
+        if (amount % MONEY_UNIT != 0) {
+            throw new IllegalArgumentException("1000원 단위로만 구매할 수 있습니다");
+        }
+    }
 
 }
