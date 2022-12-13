f181f2b151144dd95769265dbaafbfdf45a8caf8
이건회 <rg970604@naver.com>
2022-12-13 21:02:15 +0900

    refactor : GenerateLottoNumFixed.java

--- src/test/java/strategy/GenerateLottoNumFixed.java
+++ src/test/java/strategy/GenerateLottoNumFixed.java
@@ -1,16 +1,18 @@
 package strategy;
 
 import domain.strategy.GenerateLottoNum;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class GenerateLottoNumFixed implements GenerateLottoNum {
 
   @Override
   public List<Integer> generate() {
-    int [] LottoNum = {1,2,3,4,5,6};
-    return Arrays.stream(LottoNum).boxed().collect(Collectors.toList());
+    List<Integer> lottoBalls = new ArrayList<>();
+    IntStream.rangeClosed(1,6).forEach(
+        i -> lottoBalls.add(i)
+    );
+    return lottoBalls;
   }
 }
