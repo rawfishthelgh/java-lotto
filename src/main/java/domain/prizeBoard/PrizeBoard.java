f48d34aba4dcb507592b66856a06feb10f2baf9b
이건회 <rg970604@naver.com>
2022-12-13 21:02:53 +0900

    refactor : PrizeBoard.java

--- src/main/java/domain/prizeBoard/PrizeBoard.java
+++ src/main/java/domain/prizeBoard/PrizeBoard.java
@@ -1,21 +1,20 @@
 package domain.prizeBoard;
-import java.util.function.Supplier;
 
 public enum PrizeBoard {
-  MATCH3(3, () -> 5000),
-  MATCH4(4, () -> 50000),
-  MATCH5(5, () -> 1500000),
-  MATCH6(6, () -> 2000000000);
+  MATCH3(3, 5000),
+  MATCH4(4, 50000),
+  MATCH5(5, 1500000),
+  MATCH6(6, 2000000000);
 
   private final int matchNum;
-  private final Supplier<Integer> lottoPrizeMatching;
+  private final int money;
 
-  public Integer matchPrize() {
-    return this.lottoPrizeMatching.get();
+  PrizeBoard(int matchNum, int money) {
+    this.matchNum = matchNum;
+    this.money = money;
   }
 
-  PrizeBoard(int matchNum, Supplier<Integer> lottoPrizeMatching) {
-    this.matchNum = matchNum;
-    this.lottoPrizeMatching = lottoPrizeMatching;
+  public Integer matchPrize() {
+    return this.money;
   }
 }
